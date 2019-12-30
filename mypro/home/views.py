1159af9e8c122b932282e790b5d9b9fb285b46ca
ujithendrakumar <ujk222@gmail.com>
2019-12-30 16:20:12 +0530

    Initial Project Commit

---
+++ mypro/home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+# Create your views here.
+def home(request):
+    return render(request,'home/home.html')
+
+def about(request):
+    return render(request,'home/about.html')
