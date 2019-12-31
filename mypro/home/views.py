dc56d1d2ff97f39a0cf838ae15a5841b6d00fc60
ujithendrakumar <ujk222@gmail.com>
2019-12-31 16:49:06 +0530

    created new test method to pass the perameters

--- mypro/home/views.py
+++ mypro/home/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render(request,'home/home.html')
 
-def about(request):
-    return render(request,'home/about.html')
+def about(request ):
+    return render(request,'home/about.html',ls)
+
+def test(request,name):
+    ls = {'name':name}
+    return render(request,'home/test.html',ls)
